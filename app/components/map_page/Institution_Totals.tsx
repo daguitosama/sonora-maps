85efac190b35e94cab30fd1e77666cba3779c11a
Dago <tocarralero@gmail.com>
2024-05-01 08:19:04 -0400

    styles enhance

--- app/components/map_page/Institution_Totals.tsx
+++ app/components/map_page/Institution_Totals.tsx
@@ -13,54 +13,63 @@ export function Institution_Totals({ institution }: Institution_Totals_Props) {
                     <div className='max-w-[12rem] text-wrap'>
                         <p className='text-xs  '>{totals_note}</p>
                     </div>
-                    <div className=' text-black px-2 py-1 text-md font-bold bg-gradient-to-r from-white to-transparent'>
-                        <p>Nacional</p>
-                    </div>
-                    <div className=' text-black px-2 py-1 text-md font-bold bg-gradient-to-r from-white to-transparent'>
-                        <p>Local</p>
+                    <div className='grid gap-2 pb-4'>
+                        <div className=' text-black px-2 py-1 text-md font-bold bg-gradient-to-r from-white to-transparent'>
+                            <p>Nacional</p>
+                        </div>
+                        <div className=' text-black px-2 py-1 text-md font-bold bg-gradient-to-r from-white to-transparent'>
+                            <p>Local</p>
+                        </div>
                     </div>
                 </div>
                 {/* matriculados column */}
-                <div className='grid col-span-2 gap-2'>
+                <div className='grid col-span-2 '>
                     <div className='flex flex-col items-center justify-end'>
                         <p className='uppercase font-light'>Matriculados</p>
                     </div>
-                    <div className='bg-[#A8544D] rounded-md font-light text-center grid grid-rows-2'>
-                        <div className='bg-[#960E53] rounded-md py-1'>
-                            {totals.national.enrollment ? totals.national.enrollment : "-"}
-                        </div>
-                        <div className='py-1'>
-                            {totals.local.enrollment ? totals.local.enrollment : "-"}
+                    <div className='pt-[28px]'>
+                        <div className=' bg-[#A8544D] rounded-md font-light text-center grid grid-rows-2'>
+                            <div className='bg-[#960E53] rounded-md py-2'>
+                                {totals.national.enrollment ? totals.national.enrollment : "-"}
+                            </div>
+                            <div className='py-2'>
+                                {totals.local.enrollment ? totals.local.enrollment : "-"}
+                            </div>
                         </div>
                     </div>
                 </div>
 
                 {/* titulados column */}
-                <div className='grid col-span-2 gap-2'>
+                <div className='grid col-span-2 '>
                     <div className='flex flex-col items-center justify-end'>
                         <p className='uppercase font-light'>Titulados</p>
                     </div>
-                    <div className='bg-[#E3974E] rounded-md font-light text-center grid grid-rows-2'>
-                        <div className='bg-[#EFA151] rounded-md py-1'>
-                            {totals.national.entitlements ? totals.national.entitlements : "-"}
-                        </div>
-                        <div className='py-1'>
-                            {totals.local.entitlements ? totals.local.entitlements : "-"}
+                    <div className='pt-[28px]'>
+                        <div className='bg-[#E3974E] rounded-md font-light text-center grid grid-rows-2'>
+                            <div className='bg-[#EFA151] rounded-md py-2'>
+                                {totals.national.entitlements ? totals.national.entitlements : "-"}
+                            </div>
+                            <div className='py-2'>
+                                {totals.local.entitlements ? totals.local.entitlements : "-"}
+                            </div>
                         </div>
                     </div>
                 </div>
 
                 {/* titulados column */}
-                <div className='grid col-span-2 gap-2'>
+                <div className='grid col-span-2 '>
                     <div className='flex flex-col items-center justify-end'>
                         <p className='uppercase font-light'>Egresados</p>
                     </div>
-                    <div className='bg-[#B79885] rounded-md font-light text-center grid grid-rows-2'>
-                        <div className='bg-[#A7A5A6] rounded-md py-1'>
-                            {totals.national.egress ? totals.national.egress : "-"}
-                        </div>
-                        <div className='py-1'>
-                            {totals.local.egress ? totals.local.egress : "-"}
+
+                    <div className='pt-[28px]'>
+                        <div className='bg-[#B79885] rounded-md font-light text-center grid grid-rows-2'>
+                            <div className='bg-[#A7A5A6] rounded-md py-2'>
+                                {totals.national.egress ? totals.national.egress : "-"}
+                            </div>
+                            <div className='py-2'>
+                                {totals.local.egress ? totals.local.egress : "-"}
+                            </div>
                         </div>
                     </div>
                 </div>
